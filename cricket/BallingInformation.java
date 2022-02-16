cc704c6b7ace1b24f07aa652daf331871c66e89a
Rupesh khattar <70324322+flash2198@users.noreply.github.com>
2022-02-16 16:35:59 +0530

    Add files via upload

---
+++ cricket/BallingInformation.java
@@ -0,0 +1,36 @@
+package cricket;
+
+class BallingInformation {
+  private int oversBalled;
+  private int runsGiven;
+  private int wickets;
+
+  public void setOversBalled(int over) {
+    oversBalled += over;
+  }
+
+  public void setRunsGiven(int runs) {
+    runsGiven += runs;
+  }
+
+  public void setWickets(int wicket) {
+    wickets += wicket;
+  }
+
+  public int getOversBalled() {
+    return oversBalled;
+  }
+
+  public int getWickets() {
+    return wickets;
+  }
+
+  public int getRunsGiven() {
+    return runsGiven;
+  }
+
+  @Override
+  public String toString() {
+    return oversBalled + " " + wickets + " " + runsGiven;
+  }
+}
---
+++ cricket/ScoreBoard.java
@@ -0,0 +1,85 @@
+package cricket;
+
+import java.util.Map;
+
+public class ScoreBoard {
+  private String manOfTheMatch;
+  int highestRunsScored = 0;
+
+  public static ScoreBoard of() {
+    return new ScoreBoard();
+  }
+
+  private ScoreBoard() {}
+
+  public void getBattersPerformance(
+      String teamName,
+      Map<Integer, BattingInformation> battersList,
+      Map<Integer, String> idToNames,
+      int totalRunsScored) {
+
+    System.out.println("Total runs scored by the team:" + totalRunsScored);
+    System.out.println(teamName + " scoreboard:");
+    for (Integer id : battersList.keySet()) {
+      if (battersList.get(id) != null) {
+
+        if (highestRunsScored < battersList.get(id).getRunsScored()) {
+          highestRunsScored = battersList.get(id).getRunsScored();
+          manOfTheMatch = idToNames.get(id);
+        }
+
+        System.out.println(
+            idToNames.get(id)
+                + ":"
+                + " runsscored:"
+                + battersList.get(id).getRunsScored()
+                + " ballsplayed:"
+                + battersList.get(id).getBallsPlayed()
+                + " strikerate:"
+                + battersList.get(id).getStrikeRate());
+      } else {
+        System.out.println(idToNames.get(id) + ": not played");
+      }
+    }
+  }
+
+  public void getBallersPerformance(
+      String teamName,
+      Map<Integer, BallingInformation> ballersList,
+      Map<Integer, String> idToNames) {
+    System.out.println("Balling details of:" + teamName);
+    for (Integer id : ballersList.keySet()) {
+      if (ballersList.get(id) != null) {
+        System.out.println(
+            idToNames.get(id)
+                + ":"
+                + " runsGiven:"
+                + ballersList.get(id).getRunsGiven()
+                + " oversBalled:"
+                + ballersList.get(id).getOversBalled()
+                + " wickets taken:"
+                + ballersList.get(id).getWickets());
+      }
+    }
+  }
+
+  public void getResultOfMatch(
+      int firstTeamRuns, String firstTeamName, int secondTeamRuns, String secondTeamName) {
+    if (firstTeamRuns > secondTeamRuns) {
+      System.out.println(
+          firstTeamName + " team's wins by " + (firstTeamRuns - secondTeamRuns) + " runs");
+      return;
+    }
+
+    if (firstTeamRuns == secondTeamRuns) {
+      System.out.println("The match is draw");
+      return;
+    }
+
+    System.out.println(secondTeamName + " team's wins the match");
+  }
+
+  public void showManOfTheMatch() {
+    System.out.println("Man ofthe match is:" + manOfTheMatch);
+  }
+}
