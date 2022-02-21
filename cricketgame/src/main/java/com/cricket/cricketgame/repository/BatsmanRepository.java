6137222d6406c132a8cddce69dde3eb4f82232ac
Rupesh khattar <70324322+flash2198@users.noreply.github.com>
2022-02-21 13:01:47 +0530

    Delete cricketgame/src/main/java/com/cricket/cricketgame directory

--- cricketgame/src/main/java/com/cricket/cricketgame/repository/BatsmanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cricket.cricketgame.repository;
-
-import com.cricket.cricketgame.entity.Batsman;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BatsmanRepository extends JpaRepository<Batsman,Long> {
-    public Batsman findByBatsmanName(String batsmanName);
-    public Batsman findByBatsmanNameIgnoreCase(String batsmanName);
-}
--- cricketgame/src/main/java/com/cricket/cricketgame/repository/MatchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cricket.cricketgame.repository;
-
-import com.cricket.cricketgame.entity.MatchDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchRepository extends JpaRepository<MatchDetails,Long> {
-}
